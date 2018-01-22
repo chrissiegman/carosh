815ba31f6568c6e7b068679432f5fc21c4da03f4
Chris Siegman <christopher.siegman@gmail.com>
2018-01-22 12:18:04 -0600

    Updated version that handles end of string punctuation

--- code/simple_reverse.php
+++ code/simple_reverse.php
@@ -2,8 +2,6 @@
 
 // TODO Add a few unit tests!!!
 
-// TODO handle end of string punctuation
-
 // Default string to transform
 $subject = "This is the default string.";
 
@@ -12,7 +10,15 @@ if (array_key_exists(1, $argv)) {
 	$subject = $argv[1];
 }
 
-$subject = implode(" ", array_reverse(explode(" ", $subject)));
+$terminator = "";
+$end = substr($subject, -1);
+if (in_array($end, [".", "?"])) {
+	$subject = str_split($subject);
+	$terminator = array_pop($subject);
+	$subject = implode("", $subject);
+}
+
+$subject = implode(" ", array_reverse($subject = explode(" ", $subject))) . $terminator;
 var_dump($subject);
 
 ?>
