fb0e1d730de1c187f1faec8b10f9b9107e0f5e6f
Chris Siegman <christopher.siegman@gmail.com>
2018-01-22 21:43:59 +0000

    Fix syntax error, rename table in lowercase

--- simple_forms/persons.sql
+++ simple_forms/persons.sql
@@ -1,6 +1,6 @@
-CREATE TABLE Persons (
+CREATE TABLE IF NOT EXISTS persons (
     id int NOT NULL AUTO_INCREMENT,
     first_name varchar(30) NOT NULL,
     last_name varchar(30) NOT NULL,
-    PRIMARY_KEY (id)
+    PRIMARY KEY (id)
 );
