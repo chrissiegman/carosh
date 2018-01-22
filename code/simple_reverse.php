ad1943ff5e13ddcea77efd0de95de81b44a377c7
Chris Siegman <christopher.siegman@gmail.com>
2018-01-22 12:27:01 -0600

    Refactor functionality into functions

--- code/simple_reverse.php
+++ code/simple_reverse.php
@@ -10,15 +10,30 @@ if (array_key_exists(1, $argv)) {
 	$subject = $argv[1];
 }
 
-$terminator = "";
-$end = substr($subject, -1);
-if (in_array($end, [".", "?"])) {
-	$subject = str_split($subject);
-	$terminator = array_pop($subject);
-	$subject = implode("", $subject);
+var_dump(reverse_string($subject));
+
+function reverse_string($subject)
+{
+	$split = get_punctuation($subject);
+	$subject = $split['subject'];
+	$punctuation = $split['punctuation'];
+	$result = implode(" ", array_reverse($subject = explode(" ", $subject))) . $punctuation;
+	return $result;
 }
 
-$subject = implode(" ", array_reverse($subject = explode(" ", $subject))) . $terminator;
-var_dump($subject);
+function get_punctuation($subject)
+{
+	$punctuation = "";
+	$end = substr($subject, -1);
+	if (in_array($end, [".", "?", "!"])) {
+		$subject = str_split($subject);
+		$punctuation = array_pop($subject);
+		$subject = implode("", $subject);
+	}
+
+	$result['subject'] = $subject;
+	$result['punctuation'] = $punctuation;
+	return $result;
+}
 
 ?>
