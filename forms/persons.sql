df15b3871cc41a57a428856d05f24f07febaf65f
Chris Siegman <christopher.siegman@gmail.com>
2018-01-23 01:18:10 +0000

    Cleanup and comment updates

--- forms/persons.sql
+++ forms/persons.sql
@@ -4,6 +4,3 @@ CREATE TABLE IF NOT EXISTS persons (
     last_name varchar(30) NOT NULL,
     PRIMARY KEY (id)
 );
-
-SELECT * FROM User WHERE User_id IN
-(SELECT DISTINCT(User_id) FROM `UserToRole`)
